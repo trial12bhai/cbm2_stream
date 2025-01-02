4cae3c9d2804d1d4d59dd926a246fd607099fe08
trial12bhai <trial12bhai@gmail.com>
2025-01-02 20:04:25 +0530

    Update cbm3.py

--- cbm3.py
+++ cbm3.py
@@ -67,7 +67,7 @@ def on_message(client, userdata, message):
     # Function to update the plot on Streamlit
 #def update_plot():
     # Create the plot in Streamlit
-   # st.write("### Sensor Data")
+    st.write("### Sensor Data")
     
     fig, ax = plt.subplots(2, 1, figsize=(10, 6))
     
