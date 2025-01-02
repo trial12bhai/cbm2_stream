2f7208a101e718e94cb04650aa8ff329f1c137c0
trial12bhai <trial12bhai@gmail.com>
2025-01-02 20:41:46 +0530

    Update cbm3.py

--- cbm3.py
+++ cbm3.py
@@ -64,8 +64,8 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 # Start MQTT loop in a background thread
-#client.loop_start()
-client.loop_forever(timeout=1.0)
+client.loop_start()
+#client.loop_forever(timeout=1.0)
 
 # Streamlit app interface
 st.title("Real-time Sensor Data Visualization")
@@ -98,4 +98,4 @@ while True:
         # Render the updated graph
         st.pyplot(fig)
 
-    time.sleep(0.1)  # Add a small delay for smoother updates
+    time.sleep(5)  # Add a small delay for smoother updates
